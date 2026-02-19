1c86c59f683722c45f9b2875a362808c7d9426f6
boitenge <jeromeboitenge@gmail.com>
2026-02-19 16:16:01 +0200

    fg

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.SUPER_ADMIN)
+    @Get()
+    async findAll() {
+        // This method doesn't exist in service yet, need to implement or mock
+        // return this.usersService.findAll();
+        return [];
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.SUPER_ADMIN, Role.MODERATOR)
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return this.usersService.findOne(id);
+    }
 }
