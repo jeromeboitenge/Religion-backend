bc038c995052760e67a4453b444a9ce3459c59d2
boitenge <jeromeboitenge@gmail.com>
2026-02-19 15:57:10 +0200

    adding modules

--- src/churches/churches.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('churches')
-export class ChurchesController {}
--- src/churches/churches.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChurchesService } from './churches.service';
-import { ChurchesController } from './churches.controller';
-
-@Module({
-  providers: [ChurchesService],
-  controllers: [ChurchesController]
-})
-export class ChurchesModule {}
