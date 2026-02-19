698808a88233def2d503ce7f9676f10f41b48812
boitenge <jeromeboitenge@gmail.com>
2026-02-19 16:21:28 +0200

    w

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ChurchesModule } from './modules/churches/churches.module';
+import { SermonsModule } from './modules/sermons/sermons.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, ChurchesModule],
+  imports: [PrismaModule, AuthModule, UsersModule, ChurchesModule, SermonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
