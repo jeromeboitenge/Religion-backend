185e0228ad1f0e49b163467708c67958c7b9e0d1
boitenge <jeromeboitenge@gmail.com>
2026-02-19 15:55:23 +0200

    adding

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
+@Global()
 @Module({
+  controllers: [UsersController],
   providers: [UsersService],
-  controllers: [UsersController]
+  exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
